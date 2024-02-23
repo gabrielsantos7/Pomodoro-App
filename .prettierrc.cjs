934916b5c28f2a758e9c69eeba8e747109aada80
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-02-23 20:25:26 -0300

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  arrowParens: 'always', // Parêntese em arrow functions
+  bracketSpacing: true, // Espaçamento de colchetes
+  endOfLine: 'auto', // Final da linha (LF para Unix, CRLF para Windows)
+  htmlWhitespaceSensitivity: 'ignore', // Espaços em branco no HTML
+  insertPragma: false, // Desabilita correção automática
+  jsxSingleQuote: false, // Aspas simples em JSX
+  printWidth: 80, // Largura para quebrar a linha
+  proseWrap: 'always', // Quebras de linha em blocos de texto longos
+  quoteProps: 'as-needed', // Estilo de nomenclatura de propriedades de objetos
+  requirePragma: false, // Só formata se houver pragma
+  semi: true, // Ponto e vírgula no final de cada linha
+  singleQuote: true, // Aspas simples no escopo geral
+  tabWidth: 2, // Tamanho do espaço
+  trailingComma: 'none', // Vírgula no final do último elemento
+  useTabs: false, // Usa tabs em vez de espaço
+  embeddedLanguageFormatting: 'off' // Linguagens incorporadas
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-red-600 dark:text-lime-500'>Hello Vite</h1>
+    </div>
+  );
+}
