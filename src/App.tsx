4f8acf55b1acad55c5d4b0ec10fa537ee59248db
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-02-24 07:52:41 -0300

    ✨ feat: Add secondsToTime

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import PomodoroTimer from './components/pomodoro-timer';
 export default function App() {
   return (
     <div>
-      <PomodoroTimer defaultPomodoroTime={600} />
+      <PomodoroTimer defaultPomodoroTime={3700} />
     </div>
   );
 }
--- src/components/pomodoro-timer/index.tsx
+++ src/components/pomodoro-timer/index.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import { PomodoroTimerProps } from '../../models';
 import { useInterval } from '../../hooks/useInterval';
+import { secondsToTime } from '../../utils/seconds-to-time';
 
 export default function PomodoroTimer(props: PomodoroTimerProps) {
   const [mainTime, setMainTime] = useState(props.defaultPomodoroTime);
 
   useInterval(() => {
-    setMainTime(mainTime + 1)
+    setMainTime(mainTime - 1)
   }, 1000)
   return <div>
-    {mainTime}
+    {secondsToTime(mainTime)}
   </div>;
 }
