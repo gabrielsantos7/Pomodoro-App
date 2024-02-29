dc90f49a2e1bc54eec03211c96e96e618d76863e
Gabriel Silva Santos <gabrielss432112@gmail.com>
2024-02-29 19:40:45 -0300

    ✨ feat: Change icon and title

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteFaviconsPlugin('src/assets/Logo.png')]
-});
+  plugins: [react()],
+})
